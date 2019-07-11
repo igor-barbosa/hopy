a2f28509c6b72d9545179e89516e36e8406ded12
Matheus <matheuschlosser2@gmail.com>
2019-07-11 20:42:38 -0300

    Criação métodos para validação de string
    Faltou testar o método equal

--- src/library/types/TypeString.ts
+++ src/library/types/TypeString.ts
@@ -19,12 +19,42 @@ export class TypeString extends Types {
         return this;
     }
 
+    public max(data: number) {
 
+        this.commons.max = async (field: IField) => {
+            const verify = (field.value.toString().trim().length > data)
+            if(this.checkRequirements(field) && verify) {
+                return this.applyError(
+                    'max',
+                    `Deve ter no máximo ${data} caracteres.`,
+                    `O campo ${field.label || field.path} contém uma quantidade de caracteres superior a máxima`
+                )
+            }
+        }
+
+        return this
+    }
+
+    public min(data: number) {
+
+        this.commons.min = async (field: IField) => {
+            const verify = (field.value.toString().trim().length < data)
+            if(this.checkRequirements(field) && verify) {
+                return this.applyError(
+                    'min',
+                    `Deve ter no mínimo ${data} caracteres.`,
+                    `O campo ${field.label || field.path} contém uma quantidade de caracteres superior a máxima`
+                )
+            }
+        }
+
+        return this
+    }
 
     public between(min: number, max: number) {
 
         this.commons.between = async (field: IField) => {
-            const verify = (field.value.toString().trim().length < min || field.value.toString().trim().length > max);
+            const verify = (field.value.toString().trim().length < min || field.value.toString().trim().length > max)
             if(this.checkRequirements(field) && verify) {
                 return this.applyError(
                     'between',
@@ -37,6 +67,44 @@ export class TypeString extends Types {
             }
         };
 
-        return this;
+        return this
     }
+
+    public email(){
+
+        this.commons.email = async(field: IField) => {
+            const regex =  /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
+            const verify = new RegExp(regex).test(field.value)
+            if (this.checkRequirements(field) && !verify){
+                return this.applyError(
+                    'email',
+                    'E-mail inválido',
+                        `O campo ${field.label || field.path} contém um valor inválido`
+
+                )
+            }
+        }
+
+        return this
+    }
+
+    public equal(data:string){
+        this.commons.equal = async(field: IField) => {
+            const verify = (field.value === data)
+            if (this.checkRequirements(field) && verify){
+                return this.applyError(
+                    'equal',
+                    'Valor inválido',
+                    `O campo ${field.label || field.path} contém um valor diferente dos definidos`
+
+                )
+            }
+        }
+
+        return this
+    }
+
+    //TODO: CRIAR O MÉTODO EQUAL()
+    //TODO: CRIAR O MÉTODO ALLOW
+
 }
